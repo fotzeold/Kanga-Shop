e16767442e6a6a73f13d11261ff805aab0c9ee6e
Roman Popovych <mechenuyzzzz@gmail.com>
2023-01-08 11:39:03 +0200

    start project

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import Header from "../Header/Header";
+
+const App = () => {
+	return (
+		<>
+			<Header></Header>
+		</>
+	)
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import './header.scss';
+
+const Header = () => {
+	return (
+		<header>
+			<div className="container">
+				<h1>
+					Kanga.shop accounting
+				</h1>
+			</div>
+		</header>
+	)
+}
+
+export default Header;
